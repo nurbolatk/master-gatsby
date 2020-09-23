314f3269089ee3dc88c9eafb9467661e32d44e08
nurbolatk <nurbolattk@gmail.com>
2020-09-23 23:57:03 +0600

    Create global styles

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Layout from './src/components/Layout'
 
+import 'normalize.css'
+
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>
 }
--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import GlobalStyles from '../styles/GlobalStyles'
+import Typography from '../styles/Typography'
 import Footer from './Footer'
 import Nav from './Nav'
 
 export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
