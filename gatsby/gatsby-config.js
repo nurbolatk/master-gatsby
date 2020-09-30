56def0666238d26ab50541ff8ea0264bda4cf9cc
nurbolatk <nurbolattk@gmail.com>
2020-09-30 23:13:56 +0600

    Connect to local sanity cms

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,23 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env' })
+
+export default {
+  siteMetadata: {
+    title: 'Slick Slices',
+    siteUrl: 'https://www.gatsby.pizza',
+    description: 'The best pizza in Komo!',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'xn8fjoej',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+}
