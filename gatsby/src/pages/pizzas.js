9ac1ace3039943ed1042cc9152984f33b94b3c78
nurbolatk <nurbolattk@gmail.com>
2020-10-05 19:05:06 +0600

    Query pizza list and display it

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,5 +1,36 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import PizzaList from '../components/PizzaList'
 
-const PizzasPage = () => <>pizzas</>
+const PizzasPage = ({ data }) => {
+  const pizzas = data.pizzas.nodes
+  return <PizzaList pizzas={pizzas} />
+}
 
 export default PizzasPage
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        price
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
